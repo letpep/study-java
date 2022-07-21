5e86cffeae32aa0e315d77e6eb14d4ba67b1385d
lx <lixin03@benlai.com>
2022-07-21 21:01:15 +0800

    Ribbon 模块添加

---
+++ nacos-ribbon/src/main/java/com/letpep/nacosribbon/NacosRibbonApplication.java
@@ -0,0 +1,13 @@
+package com.letpep.nacosribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosRibbonApplication.class, args);
+    }
+
+}
---
+++ nacos-ribbon/src/test/java/com/letpep/nacosribbon/NacosRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letpep.nacosribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
