735170201847f09d069725413941953ab4906cc5
lx <lixin03@benlai.com>
2022-08-27 12:39:14 +0800

    初始化08.01

---
+++ mq-provider/src/test/java/com/letpep/mqprovider/MqProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letpep.mqprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
