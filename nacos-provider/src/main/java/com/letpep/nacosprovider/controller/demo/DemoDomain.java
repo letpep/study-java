fdf67eeecd06952401ceb79c301e82709e20daa8
lx <lixin03@benlai.com>
2022-08-29 01:29:18 +0800

    算法测试类

---
+++ nacos-provider/src/main/java/com/letpep/nacosprovider/controller/demo/DemoDomain.java
@@ -0,0 +1,20 @@
+package com.letpep.nacosprovider.controller.demo;
+
+public class DemoDomain {
+    public static void main(String[] args) {
+//        String a ="abc";
+//        String b = "abc";
+//        System.out.println(a==b);
+//        System.out.println(a.equals(b));
+//        String c = new String ("abc");
+//        String d = new String ("abc");
+//        System.out.println(a==c);
+//        System.out.println(a.equals(c));
+//        System.out.println(c==d);
+        int a =10000;
+        Integer b = 1000;
+        Integer c =1000;
+
+        System.out.println(b.equals(c));
+    }
+}
